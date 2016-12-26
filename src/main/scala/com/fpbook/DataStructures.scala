8d2469e2e8c6533e0f212bd717727d60eb1bf91f
PeterBergmann73 <pbergmann73@yahoo.com>
2016-12-26 17:50:01 +0100

    Further work on the data structures.

--- src/main/scala/com/fpbook/DataStructures.scala
+++ src/main/scala/com/fpbook/DataStructures.scala
@@ -3,22 +3,22 @@ package com.fpbook
 
 object DataStructures {
 
-  def binarySearch[T](a: Array[T], key: T, comparator: (T, T) => Boolean): Int = {
+  def binarySearch[T](as: Array[T], key: T, gt: (T, T) => Boolean): Int = {
 
     @annotation.tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
       if (low > high) -mid - 1
       else {
         val mid2 = (low + high) / 2
-        val d = a(mid2)
-        if (d == key) mid2
-        else if (comparator(d, key))
-          go(low, mid2, mid2 - 1)
+        val a = as(mid2)
+        val greater = gt(a, key)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
 
-    go(0, 0, a.length - 1)
+    go(0, 0, as.length - 1)
   }
 
 }
